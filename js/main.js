d180977a086721b0353e9b1f39c54fa0f5eb2ac2
kbatuigas <kbatuigas@gmail.com>
2018-10-19 07:00:34 -0700

    ARIA attribute for View Details button

--- js/main.js
+++ js/main.js
@@ -181,6 +181,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label', `View details for ${restaurant.name}`);
   more.href = DBHelper.urlForRestaurant(restaurant);
   restaurantDetail.append(more);
 
