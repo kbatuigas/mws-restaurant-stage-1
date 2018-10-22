4743b597f7231ee9f6b0b32717b37f4d6ebaca31
kbatuigas <kbatuigas@gmail.com>
2018-10-22 00:04:12 -0700

    Add service worker
    Service worker registration in separate JS file.
    Resource: https://developers.google.com/web/fundamentals/primers/service-workers/

---
+++ js/register_sw.js
@@ -0,0 +1,10 @@
+// Register service worker when page finishes loading
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        console.log('ServiceWorker registration successful');
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
